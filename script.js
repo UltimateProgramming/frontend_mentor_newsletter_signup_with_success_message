883cd2c6826bd86a8d982abd0dfb74312fcec678
Tim <this-is-not-my-real-email@test.com>
2023-12-27 22:17:40 +0100

    added functionality and corrected some stylings

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let emailAddress = "test";
+
 function applyErrorWhenWrongEmailPattern(element) {
   let text = element.value;
   const regex = new RegExp(
     /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/
   );
-  console.log(regex.test(regex));
   if (regex.test(text)) {
     element.classList.remove("error");
     document.getElementById("subscribe").removeAttribute("disabled");
@@ -11,4 +12,13 @@ function applyErrorWhenWrongEmailPattern(element) {
   }
   element.classList.add("error");
   document.getElementById("subscribe").setAttribute("disabled", "");
+}
+
+function onSubscribeClick() {
+  emailAddress = document.getElementById("email").value;
+  let successCard = document.getElementsByClassName("success-card")[0];
+  successCard.style.display = "grid";
+  let text = successCard.getElementsByTagName("p")[0];
+  text.innerText = text.innerText.replace("{email}", emailAddress);
+  document.getElementsByClassName("card")[0].remove();
 }
